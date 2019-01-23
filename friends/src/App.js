6e839b1a3b0dcca956f4bf488e03f3c2585f2ec6
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-23 16:03:44 -0500

    Create components and display friend data, set error messaging

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import FriendsList from './components/FriendsList';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: [],
+    error: ""
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friend')
+      .then(res => this.setState({friends: res.data}))
+      .catch(err => this.setState({error: "There has been an error."}));
+  }
   render() {
+    console.log(this.state.error);
     return (
       <div className="App">
-        
+      {this.state.error && `${this.state.error}`}
+        <FriendsList friends={this.state.friends} />
       </div>
     );
   }
